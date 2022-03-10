f0c1588423d37d7ae4f1b289437c73c7ef4dd9f7
jeth318 <jesper.thornberg@me.com>
2022-03-11 00:11:50 +0100

    Refactor

--- app/Program.cs
+++ app/Program.cs
@@ -5,7 +5,14 @@
     {
         static void Main(string[] args)
         {
-            Process.Start("shutdown","/s /t 0");
+            Console.WriteLine("Hejjo");
+            Process cmd = new Process();
+            cmd.StartInfo.FileName = "cmd.exe";
+            // Delay shutdown with 20 seconds to enable user to cancel with 'shutdown -a' in separate cmd.
+            cmd.StartInfo.Arguments = "/c shutdown /s /t 20";
+            cmd.StartInfo.CreateNoWindow = true;
+            cmd.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            cmd.Start();
         }
     }
 }
--- server/server.py
+++ server/server.py
@@ -1,17 +1,37 @@
+from cmath import exp
+from msilib.schema import Error
 from dotenv import load_dotenv
-import flask
+from flask import Flask, jsonify, make_response
+import subprocess
 import os
 load_dotenv()
-app = flask.Flask(__name__)
+
+server = Flask(__name__)
 port = os.environ["PORT"]
 host = os.environ["HOST"]
+dirname = os.path.dirname(__file__)
+appPath = "../app/bin/Debug/net6.0/shutdowner.exe"
+
 load_dotenv()
 
-@app.route('/poweroff', methods=['POST'])
-def home():
-    os.system("C:/Users/jeth/dev/shutdowner/app/bin/Debug/net6.0/shutdowner.exe")
-    print("Attempting to shutdown the PC...")
-    return "HEY"
+def sendErrorResponse(e):
+    exceptionMessage = "Failed to initiate shutdown. Reason: " + e
+    print(exceptionMessage)
+    return make_response(jsonify(success=0,message=exceptionMessage), 500)
+
+@server.route('/poweroff', methods=['POST'])
+def index():
+    try:
+        absAppPath = os.path.join(dirname, appPath).replace(os.sep, "/")
+        subprocess.run(absAppPath, check = True)
+    except Exception as e:
+        return sendErrorResponse(str(e))
+
+    sucessMessage = "Shutdown initiated"
+    print(sucessMessage)
+    return make_response(jsonify(success=1,message=sucessMessage), 200)
+
+server.run(host=host, port=port)
 
 
-app.run(host=host, port=port)
+
